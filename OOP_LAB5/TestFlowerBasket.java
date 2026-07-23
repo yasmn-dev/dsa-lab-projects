880bdbcddf99cf8a8f4350d6bd47c7f109c0ec30
Yasemin Nisa Ergel <234663555+yasmn-dev@users.noreply.github.com>
2026-07-23 22:03:15 +0300

    Delete OOP_LAB5/TestFlowerBasket.java

--- OOP_LAB5/TestFlowerBasket.java
+++
@@ -1,7 +0,0 @@
-public class TestFlowerBasket {
-    public static void main(String[] args) {
-        Flower flower = new Flower("pink", "rose", 1.5);
-        Bouquet bouquet = new Bouquet("Valentine's Day", flower, 13);
-        System.out.println("Cost of the bouquet: " + bouquet.getCost());
-    }
-}
